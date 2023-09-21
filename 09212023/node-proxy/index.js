8d79e48dda2f25f98296266119608c09b1bdb9f1
jin <jin@caixingxing.com>
2023-09-21 19:00:18 +0800

    修改代理链接

--- 09212023/node-proxy/index.js
+++ 09212023/node-proxy/index.js
@@ -7,7 +7,7 @@ const proxy = httpProxy.createProxyServer({});
 // 监听端口
 const server = http.createServer((req, res) => {
     // 将请求代理到目标服务器
-    proxy.web(req, res, { target: 'http://your_backend_server' });
+    proxy.web(req, res, { target: 'https://api.openai-proxy.com' });
 });
 
 // 监听端口
