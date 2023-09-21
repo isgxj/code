a7e977652f499c4943037069289c8760f7941669
jin <jin@caixingxing.com>
2023-09-21 18:59:16 +0800

    增加node反向代理

---
+++ 09212023/node-proxy/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// 创建反向代理服务器
+const proxy = httpProxy.createProxyServer({});
+
+// 监听端口
+const server = http.createServer((req, res) => {
+    // 将请求代理到目标服务器
+    proxy.web(req, res, { target: 'http://your_backend_server' });
+});
+
+// 监听端口
+const port = 3005;
+server.listen(port, () => {
+    console.log(`反向代理服务器已启动，监听端口 ${port}`);
+});
